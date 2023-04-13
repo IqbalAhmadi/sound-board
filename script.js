13c3acc504579ec8eeba391f9b105938858f29ff
Mohammad Ahmadi <iqbal_dyncorp@live.com>
2023-04-12 18:27:35 -0700

    added function to stop the sound when clicked next

--- script.js
+++ script.js
@@ -6,5 +6,21 @@ sounds.forEach((sound) => {
 
   btn.innerText = sound
 
+  btn.addEventListener('click', () => {
+    stopSongs() // this stops the sound when we click on the next one
+
+    document.getElementById(sound).play()
+  })
+
   document.getElementById('buttons').appendChild(btn)
 })
+
+// function to stop the sound when we hit the next button.
+function stopSongs() {
+  sounds.forEach((sound) => {
+    const song = document.getElementById(sound)
+
+    song.pause()
+    song.currentTime = 0
+  })
+}
